fa54b6b6c43e8fec624dbcbd1d3e7063fea8cdaa
Sonya Kondratskaya <sonyakondratskaya@gmail.com>
2021-10-06 11:44:23 +0300

    <Required>

--- Lab3.C++/Lab3.C++.cpp
+++ Lab3.C++/Lab3.C++.cpp
@@ -5,17 +5,20 @@
 using namespace std;
 int main()
 {
-    int e,n,fac;
-    double sum;
+    int eps, n, fact,res,i;
+    double sump,sumn; //сума початкова, сума нова
     setlocale(LC_ALL, "Ukrainian");
-    cout << "Введіть значення лічильника n та значення точності е" << endl;
-    cin >>n>> e;
-    sum = 0;
-    fac = 1;
-    for (int i = 1; i <= n; i++)
-    {
-        fac = fac * i;           //факторіал і
-        sum = sum + (pow((-2), i) / fac);
-    }
-    cout << "Значення суми виразу = " << fixed << setprecision(e) << sum << endl;
+    cout << "Введіть значення точності еps" << endl;
+    cin >> eps;
+    sump = 0;
+    fact = 1; //факторіал 
+    i = 1;
+    do {
+        fact = fact * i;      
+        sumn = sump + (pow((-2), i) / fact);
+        res = sumn - sump; 
+        sump = sumn;
+        i++;
+    } while (abs(res) < eps); //визначення точності
+    cout << "Значення суми виразу = " << sumn << endl;
 }
