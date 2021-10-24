8f43235d64db99d40f589da2fe4e9ea38f445540
Sonya Kondratskaya <sonyakondratskaya@gmail.com>
2021-10-24 18:15:48 +0300

    0)

---
+++ Lab5.C++/Lab5.C++.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+    int k, a1, a2, a3, a4 ;             
+	k = 0;
+	for (int i = 1000; i < 10000; i++)
+	{
+		a1 = i / 100;
+		a2 = i % 100;
+		if (a1 == a2)
+		{
+			k += 1;
+		}
+	}
+	cout << "Кількість чотирьохзначних чисел, що представляють собою повторювані фрагменти послідовності цифр, дорівнює: " << k << endl;
+}
+
