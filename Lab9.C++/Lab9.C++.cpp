659a9d3f85c7f1f5ecbf16ad79509b006cec8f60
Sonya Kondratskaya <sonyakondratskaya@gmail.com>
2021-12-20 23:04:20 +0200

    0_0

---
+++ Lab9.C++/Lab9.C++.cpp
@@ -0,0 +1,37 @@
+﻿// Lab9 Варіант 14
+#include <iostream>
+using namespace std;
+int kolvo_slov(char s[80])
+{
+    int slovo, count = 0;
+    int i = 0;
+    while (s[i] == ' ' && s[i] != '\0')
+        i++;
+    slovo = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == ';')
+        {
+            do {
+                i++;
+                if (s[i] != ' ' && slovo == 0)
+                {
+                    slovo = 1;
+                    count++;
+                }
+                else if (s[i] == ' ')
+                    slovo = 0;
+
+            } while (s[i] != ';');
+        }
+        i++;
+    }
+    return count;
+}
+int main()
+{
+    char str[80]="hello; you sweetie little mini pig; bye";
+    cout << "Kolvo slov:   " << kolvo_slov(str);
+}
+
+
