4b2404f9eb6a7803d538cdabb8978d210bb5ec9e
Sonya Kondratskaya <sonyakondratskaya@gmail.com>
2021-12-21 17:18:04 +0200

    додаткові бали

--- Lab9.Python/Lab9.Python.py
+++ Lab9.Python/Lab9.Python.py
@@ -1,17 +1,2 @@
-s="hello; you sweetie little mini pig; bye"
-count = 0
-i=0
-while s[i] == ' ' & s[i] != '\0':
-    i+=1
-slovo=0
-while s[i] != '\0':
-    if s[i] == ';':
-         i+=1
-         while s[i] != ';':
-             if s[i] != ' ' & slovo == 0:
-                 slovo = 1
-                 count+=1
-             elif s[i] == ' ':
-                 slovo = 0
-             i+=1
-print("Kolvo slov:   ",count)
+s="hello; я учу язык программирования Python; bye"
+print("Kolvo slov:   ",len(s.split(';')[1].split()))
