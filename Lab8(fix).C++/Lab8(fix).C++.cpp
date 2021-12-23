97c25f23bc390d9a93f7fa06e47a938939cf79dc
Sonya Kondratskaya <sonyakondratskaya@gmail.com>
2021-12-24 00:45:16 +0200

    .

--- Lab8(fix).C++/Lab8(fix).C++.cpp
+++ Lab8(fix).C++/Lab8(fix).C++.cpp
@@ -61,6 +61,10 @@ int main()
     cout << "Vvedit rozmir masuva:  " << endl;
     cin >> size;
     double** arr = new double* [size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new double[size];
+    }
     input(size, arr);
     func(size, arr);
     output(size, arr);
